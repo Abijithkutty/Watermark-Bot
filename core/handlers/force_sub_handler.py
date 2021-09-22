956e6f8c3286aac89de4bc74487b102f7bf76a12
Abijithkutty <87305206+Abijithkutty@users.noreply.github.com>
2021-09-22 10:59:03 +0530

    Update force_sub_handler.py

--- core/handlers/force_sub_handler.py
+++ core/handlers/force_sub_handler.py
@@ -42,7 +42,7 @@ async def handle_force_subscribe(bot, cmd):
     except Exception:
         await bot.send_message(
             chat_id=cmd.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/linux_repo).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/REX_Bots_Support).",
             parse_mode="markdown",
             disable_web_page_preview=True
         )
